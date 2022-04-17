f114328891e5431139de655af4bed45cacc2fca6
Ariq Fraser <ariqfraser@gmail.com>
2022-04-17 18:10:20 +0100

    819

--- problems/819.js
+++ problems/819.js
@@ -7,3 +7,21 @@
 
 // For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you
 // could buy the stock at 5 dollars and sell it at 10 dollars.
+
+function calcMaxProfit(array) {
+    const arrayLength = array.arrayLength;
+    let profits = [];
+    for (let i in array) {
+        for (let ii in array) {
+            if (i <= ii) continue;
+            profits.push(array[ii] - array[i]);
+        }
+    }
+
+    profits = profits.sort((a, b) => a - b);
+
+    return profits[profits.length - 1];
+}
+
+const input = [9, 11, 8, 5, 7, 10];
+console.log(calcMaxProfit(input));
