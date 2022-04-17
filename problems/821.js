95f9f2ff690b5947b92ad4b2937dbd8c8a0d997b
Ariq Fraser <ariqfraser@gmail.com>
2022-04-17 17:56:34 +0100

    821 done but im not sure if its the fastest

--- problems/821.js
+++ problems/821.js
@@ -5,15 +5,42 @@
 
 // For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False
 
-const inputArray = [-6, 0, 2, 40];
+const fixedArray = [-6, -1, 2, 5, 12, 40];
 
-function getFixedPoint(input) {
-    for (let i in input) {
-        if (input[i] == i) {
-            return input[i];
-        }
-    }
-    return false;
+let randomArray = [];
+
+for (let x = 0; x < 100; x++) {
+    randomArray.push(Math.floor(Math.random() * 101));
 }
+randomArray = randomArray.sort((a, b) => a - b);
 
-console.log(getFixedPoint(inputArray));
+let checkedPos0 = false;
+let binarySearch = function (start, end, array) {
+    if (checkedPos0) return false;
+    if (start > end) {
+        checkedPos0 = true;
+        return binarySearch(0, 1, array);
+    }
+
+    const middleIndex = Math.floor((start + end) / 2);
+    console.log(start, end, middleIndex, array[middleIndex]);
+
+    if (array[middleIndex] === middleIndex) return array[middleIndex];
+
+    if (array[middleIndex] > middleIndex) {
+        return binarySearch(start, middleIndex - 1, array);
+    }
+
+    if (array[middleIndex] < middleIndex) {
+        return binarySearch(middleIndex + 1, end, array);
+    }
+};
+
+const input = fixedArray;
+console.log('input =>', input);
+
+console.time('time');
+
+console.log('value matches index =>', binarySearch(0, input.length, input));
+
+console.timeEnd('time');
